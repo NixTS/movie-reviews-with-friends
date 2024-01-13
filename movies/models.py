33eaa2f88f53352b787fc64c212a56960e4a9d0c
TobiS <130932230+NixTS@users.noreply.github.com>
2024-01-13 16:29:35 +0100

    Fix movie details to show correct values

--- movies/models.py
+++ movies/models.py
@@ -6,9 +6,14 @@ class MovieDetails(models.Model):
     movie_id = models.IntegerField(unique=True)
     movie_title = models.CharField(max_length=255, blank=True, null=True)
     movie_poster = models.CharField(max_length=255, blank=True, null=True)
-
+    
     def get_poster_url(self):
         if self.movie_poster:
             base_url = 'https://image.tmdb.org/t/p/w300'
             return f'{base_url}{self.movie_poster}'
         return None
+
+    movie_description = models.TextField(max_length=10000, blank=True, null=True)
+    movie_genre = models.JSONField(blank=True, null=True)
+    movie_release_date = models.DateField(blank=True, null=True)
+    movie_length = models.DurationField(blank=True, null=True)
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,7 @@
 import requests
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from datetime import datetime, timedelta
 from moviestar.settings import TMDB_API_KEY
 from .models import MovieDetails
 
@@ -14,25 +15,37 @@ def list_of_movies(request):
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        print(data)
 
         for movie in data.get('results', []):
             movie_id = movie.get('id')
             if movie_id:
                 movie_title = movie.get('title', '')
                 movie_poster = movie.get('poster_path', '')
+                movie_description = movie.get('overview', '')
+                genre_ids = movie.get('genre_ids', [])
+                movie_genre = fetch_genre_names(genre_ids)
+                movie_release_date = datetime.strptime(movie.get('release_date', ''), '%Y-%m-%d').date()
+                movie_length = timedelta(minutes=movie.get('runtime', 0))
 
                 movie_details, created = MovieDetails.objects.get_or_create(
                     movie_id=movie_id,
                     defaults={
                         'movie_title': movie_title,
                         'movie_poster': movie_poster,
+                        'movie_description': movie_description,
+                        'movie_genre': movie_genre,
+                        'movie_release_date': movie_release_date,
+                        'movie_length': movie_length,
                     }
                 )
 
                 if not created:
                     movie_details.movie_title = movie_title
                     movie_details.movie_poster = movie_poster
+                    movie_details.movie_description = movie_description
+                    movie_details.movie_genre = movie_genre
+                    movie_details.movie_release_date = movie_release_date
+                    movie_details.movie_length = movie_length
                     movie_details.save()
 
         movies = MovieDetails.objects.all()
@@ -42,7 +55,16 @@ def list_of_movies(request):
         return HttpResponse(f"Error fetching data from TMDB API. Status code: {response.status_code}", status=response.status_code)
 
 
-
 def movie_detail(request, movie_id):
     movie = get_object_or_404(MovieDetails, movie_id=movie_id)
     return render(request, 'movies/movie_details.html', {'movie': movie})
+
+
+def fetch_genre_names(genre_ids):
+    genre_url = f'{BASE_URL}genre/movie/list?api_key={TMDB_API_KEY}'
+    response = requests.get(genre_url)
+    if response.status_code == 200:
+        genres_data = response.json().get('genres', [])
+        genre_names = [genre['name'] for genre in genres_data if genre['id'] in genre_ids]
+        return genre_names
+    return []

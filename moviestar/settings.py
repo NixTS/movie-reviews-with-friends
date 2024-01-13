18ec5da646d65fd2f76809efed9ca89209f9f5df
TobiS <130932230+NixTS@users.noreply.github.com>
2024-01-13 10:01:22 +0100

    Edit code to be ready for deployment

--- moviestar/settings.py
+++ moviestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m7i*gg(@#!rnwab9=q((bh(god$0ogegncz@@_o*%fyrw79q(6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition

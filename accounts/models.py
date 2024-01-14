1f3d0665a48ccc01f4440f84b286c156ba6e574c
TobiS <130932230+NixTS@users.noreply.github.com>
2024-01-14 20:05:36 +0100

    Add user profile custom fields

---
+++ accounts/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    user_date_of_birth = models.DateField(null=True, blank=True)
+    user_bio = models.TextField(max_length=5000, blank=True, null=True)
--- moviestar/settings.py
+++ moviestar/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movies',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'

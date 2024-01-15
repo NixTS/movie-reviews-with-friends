7a31b22ebc376103321365fc3caa3cb0a3c2e3e3
TobiS <130932230+NixTS@users.noreply.github.com>
2024-01-15 20:59:05 +0100

    Refactor Reviews model structure and naming conventions

--- reviews/models.py
+++ reviews/models.py
@@ -8,13 +8,13 @@ from review_groups.models import ReviewGroups
 # Create your models here.
 
 class Review(models.Model):
-    review_id = models.IntegerField(unique=True)
+    review_id = models.AutoField(primary_key=True)
     review_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='reviews')
     review_movie = models.ForeignKey(MovieDetails, on_delete=models.CASCADE, related_name='reviews')
     review_group = models.ForeignKey(ReviewGroups, on_delete=models.CASCADE, related_name='group_reviews', null=True, blank=True)
     review_rating = models.IntegerField()
-    review_title = models.CharField(max_length=255, blank=False, null=False)
-    review_text = models.TextField(max_length=20000, blank=True, null=True)
+    review_title = models.CharField(max_length=255, blank=False, null=False, default='Provide a review title.')
+    review_text = models.TextField(max_length=20000, blank=True, null=True, default='Provide a review text.')
 
     class Meta:
         unique_together = ('review_user', 'review_movie', 'review_group')

60b3cbdf5778eee1311640df250c1f9a96b4a6c8
TobiS <130932230+NixTS@users.noreply.github.com>
2024-01-20 16:39:04 +0100

    Edit accounts urls to be more semantic

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import list_of_users, user_details, register, edit_own_profile, user_profile
 
 urlpatterns = [
-    path('users/', list_of_users, name='list_of_users'),
-    path('users/<int:id>/', user_details, name='user_details'),
+    path('all_users/', list_of_users, name='list_of_users'),
+    path('user_details/<int:id>/', user_details, name='user_details'),
     path('user_profile/', user_profile, name='user_profile'),
     path('register/', register, name='register'),
     path('edit_user_profile/', edit_own_profile, name='edit_own_profile'),

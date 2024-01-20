6625acd5ff5c9c19cb5a531f4bc453ad4e366395
TobiS <130932230+NixTS@users.noreply.github.com>
2024-01-20 16:23:42 +0100

    Add base.html and index.html

--- moviestar/urls.py
+++ moviestar/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from homepage.views import index
 
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
     path('groups/', include('review_groups.urls')),

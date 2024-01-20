359eda5133138c2358c2007031cedb7f608eba8e
TobiS <130932230+NixTS@users.noreply.github.com>
2024-01-20 16:52:24 +0100

    Edit movies urls to be more semantic

--- movies/urls.py
+++ movies/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from movies.views import list_of_movies, movie_detail, add_movie_to_group
 
 urlpatterns = [
-    path('movies/', list_of_movies, name='movies'),
-    path('movies/<int:movie_id>/', movie_detail, name='movie_details'),
+    path('list_movies/', list_of_movies, name='movies'),
+    path('details/<int:movie_id>/', movie_detail, name='movie_details'),
     path('movies/<int:movie_id>/add_to_group/', add_movie_to_group, name='add_movie_to_group'),
 ]
